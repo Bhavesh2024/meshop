06792e4f16f37f8102a3572b01848953f5c3c128
bhaveshweb17 <bhaveshwebstudio@gmail.com>
2024-11-30 11:58:14 +0530

    user api issue resolved

--- server/server.js
+++ server/server.js
@@ -8,14 +8,25 @@ const apiRoute = require ('./routes/api')
 const adminRoute = require('./routes/admin');
 const cookieParser = require("cookie-parser");
 require('dotenv').config();
-app.use(
-	cors({
-		origin: "https://meshop-sr1f.vercel.app",
-		method: "GET,POST,PUT,PATCH,DELETE",
-		allowedHeaders: ["Content-Type", "Authorization"],
-		credentials:true
-	})
-);
+const allowedOrigins = [
+  'https://meshop-sr1f.vercel.app',
+  'https://meshop-sr1f-4rdehw7bh-bhavesh2024s-projects.vercel.app',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true); // Allow the origin
+    } else {
+      callback(new Error('Not allowed by CORS')); // Deny the origin
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],  // Allowed headers
+  credentials: true,  // Enable cookies and authorization headers
+};
+
+app.use(cors(corsOptions));
 
 app.use(cookieParser())
 app.use(express.json());
